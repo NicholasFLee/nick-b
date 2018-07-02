36790f92f4b99c9ceaa2b68d1cdb7ea16dd7f860
Nick <2272750086@qq.com>
2018-07-02 18:40:18 +0800

    use  return index.html

--- routers/routers.go
+++ routers/routers.go
@@ -1,10 +1,23 @@
 package routers
 
-import "github.com/gin-gonic/gin"
+import (
+	"fmt"
+
+	"github.com/gin-gonic/gin"
+)
 
 // Routes the handlers
 func Routes(r *gin.Engine) {
 	r.GET("/article/:id", GetArticleHandler)
 	r.GET("/article", GetArticlePreviewsHandler)
 	r.POST("/article", InsertArticleHandler)
+	// fs := http.FileServer(http.Dir("/usr/share/nick/nick-f/dist/index.html"))
+	// http.Handle("/", fs)
+	r.GET("/", func(c *gin.Context) {
+		// /usr/share/nick/nick-f/dist/index.html
+		fmt.Println("////////")
+		c.File("/usr/share/nick/nick-f/dist/index.html")
+
+	})
+
 }
